0aeba4422c7b1079f1f6e0ce5ad0881633ecc6d3
Alberto <683483@unizar.es>
2020-11-10 18:20:47 +0100

    Detecta y clasifica trama motores
    He añadido la función procesarMotores que separa los datos en los diferentes campos de la trama y después el consumer los guarda en .csv

--- Recepcion_datos_python.py
+++ Recepcion_datos_python.py
@@ -14,7 +14,7 @@ Created on Sat Oct 17 10:31:13 2020
 
 #         while 1:
             
-#             usbData = ser.read(4)
+#             usbData = ser.read(10)
 #             s=' '.join(f'0x{bytes:x}' for bytes in usbData)
 #             dataFile.write(s+'\n')
             
@@ -31,20 +31,54 @@ import random
 import threading
 import time
 
-def producer(queue, event, ser):
+def sincronizarInicio(ser, queue, tamañoTrama, bitInicioTrama):
+    inicio = 0
+    while not inicio:   #busco el inicio de la trama que siempre empieza con 0x3e
+        usbData = ser.read(1)
+        if (usbData == b'\x3e'): 
+            inicio = 1; 
+            logging.info("INICIO INICIO INICIO INICIO INICIO INICIO")
+            usbData = ser.read(tamañoTrama - bitInicioTrama)
+            message = "0x3e,"+','.join(f'0x{bytes:x}' for bytes in usbData)
+            logging.info("Producer got message: %s", message)
+            queue.put(message)
+            return(1)
+        # else:
+        #     tamañoTrama = int.from_bytes(usbData, 'big') - 0x30
+
+def procesadoTramaMotores(datos):
+    logging.info("Procesando")
+    separados = datos.split(",")
+    datosBinarios = ""
+    message = ""
+    for bytes in separados:
+        datosBinarios += bytes[2:4]
+    datosBinarios = bin(int(datosBinarios,16))[2:]
+    logging.info("Bytes separados")
+    #Trama motores: 6 +12+12+12+ 4 +4+4+1+1+1+1+1+1+2+8
+    message = datosBinarios[6 :18] #12
+    message += "," + datosBinarios[18:30] #12 
+    message += "," + datosBinarios[30:42] #12 
+    message += "," + datosBinarios[46:50] #4+ 4 
+    message += "," + datosBinarios[50:54] #4
+    message += "," + datosBinarios[54:55] #1
+    message += "," + datosBinarios[55:56] #1
+    message += "," + datosBinarios[56:57] #1
+    message += "," + datosBinarios[57:58] #1
+    message += "," + datosBinarios[58:59] #1
+    message += "," + datosBinarios[59:60] #1
+    message += "," + datosBinarios[62:70] #2+ 8
+    return(message)
+
+def producer(queue, event, ser, tamañoTrama, bitInicioTrama):
     """Pretend we're getting a number from the network."""
     try:
-        
+        sincronizado = 0
         while not event.is_set():
-            inicio = 0;
-            #while not inicio:
-            usbData = ser.read(1)
-            if (usbData == b'\x3e'):
-                inicio = 1; 
-                logging.info("INICIO INICIO INICIO INICIO INICIO INICIO")
-            else:
-                tamañoTrama = int.from_bytes(usbData, 'big') - 0x30
-            message = ' '.join(f'0x{bytes:x}' for bytes in usbData)
+            if (not sincronizado):
+                sincronizado = sincronizarInicio(ser, queue, tamañoTrama, bitInicioTrama)
+            usbData = ser.read(tamañoTrama)
+            message = ','.join(f'0x{bytes:x}' for bytes in usbData)
             logging.info("Producer got message: %s", message)
             queue.put(message)
     
@@ -58,7 +92,7 @@ def consumer(queue, event, txt_file):
     """Pretend we're saving a number in the database."""
     while not event.is_set() or not queue.empty():
         message = queue.get()
-        txt_file.write(message+'\n')
+        txt_file.write(procesadoTramaMotores(message)+'\n')
         logging.info(
             "Consumer storing message: %s (size=%d)", message, queue.qsize()
         )
@@ -69,15 +103,16 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
-
-    pipeline = queue.Queue(maxsize=10)
+    tamañoTramaCOM31 = 9           #tamaño de la trama que envío yo por el USB de tamaño arbitrario
+    bitInicioTramaCOM31 = 1
+    colaCOM = queue.Queue(maxsize=10)
     event = threading.Event() 
-    with Serial('COM31', baudrate = 1_500_000, timeout = 1) as puerto_serie:
-         puerto_serie.set_buffer_size(rx_size = 100_000, tx_size = 100_000)
-         with open("datosUSB.txt" , "w") as dataFile:
-             with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-                executor.submit(producer, pipeline, event, puerto_serie)
-                executor.submit(consumer, pipeline, event, dataFile)
+    with Serial('COM31', baudrate = 1_500_000, timeout = 1) as puerto_serie31:
+          puerto_serie31.set_buffer_size(rx_size = 100_000, tx_size = 100_000)
+          with open("datosUSB.csv" , "w") as dataFile:
+              with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+                executor.submit(producer, colaCOM, event, puerto_serie31, tamañoTramaCOM31, bitInicioTramaCOM31)
+                executor.submit(consumer, colaCOM, event, dataFile)
         
                 time.sleep(10)
                 logging.info("Main: about to set event")
